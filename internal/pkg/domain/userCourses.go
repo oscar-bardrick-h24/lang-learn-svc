2c519adcd5f50c3629b931a5475a3f2e65e674f4
Oscar Bardrick <bardrick@talon.one>
2022-09-14 23:35:14 +0200

    rm completed from UserCourse

--- internal/pkg/domain/userCourses.go
+++ internal/pkg/domain/userCourses.go
@@ -6,7 +6,6 @@ type UserCourse struct {
 	UserID         string    `json:"user_id"`
 	CourseID       string    `json:"course_id"`
 	ActiveLessonID string    `json:"active_lesson_id"`
-	Completed      bool      `json:"completed"`
 	CreatedAt      time.Time `json:"created_at,omitempty"`
 	UpdatedAt      time.Time `json:"updated_at,omitempty"`
 }
